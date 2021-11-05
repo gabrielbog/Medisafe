cdec818f49154af8e1e59000e9a6c6971aeab32e
Vasile Campeanu <91255194+vasilecampeanu@users.noreply.github.com>
2021-11-05 09:05:15 +0200

    docs: Update README.md

--- src/main/java/com/medisafe/app/classes/EntryPoint.java
+++ src/main/java/com/medisafe/app/classes/EntryPoint.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class EntryPoint
 {
-    public static void main( String[] args ) throws SQLException 
+    public static void main(String[] args) throws SQLException 
     {
         Application application = new Application();
         application.run();
